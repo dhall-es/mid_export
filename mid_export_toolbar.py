64bf05a4ba2a46d4d48117042e24439569322883
dhall-es <dylan.hall777@escapestudios.ac.uk>
2026-02-13 15:53:45 +0000

    ORM Undo Group

--- mid_export_toolbar.py
+++ mid_export_toolbar.py
@@ -1,4 +1,5 @@
 import sd
+from sd.api.sdhistoryutils import SDHistoryUtils
 from sd.api.sdproperty import SDPropertyCategory
 from sd.api.sdbasetypes import float2
 from sd.api.sdvaluestring import SDValueString
@@ -65,95 +66,96 @@ class MainToolBar(QToolBar):
 
     @Slot()
     def __wireORM(self):
-        selected_nodes = self.__qtUIMgr.getCurrentGraph().getNodes()
+        with SDHistoryUtils.UndoGroup("Auto ORM"):
+            selected_nodes = self.__qtUIMgr.getCurrentGraph().getNodes()
 
-        outputNodes = nds.getOutputNodes(selected_nodes)
+            outputNodes = nds.getOutputNodes(selected_nodes)
 
-        # If no output nodes are found, return
-        if (len(outputNodes) <= 0):
-            return
+            # If no output nodes are found, return
+            if (len(outputNodes) <= 0):
+                return
 
-        annotation = SDPropertyCategory(0)
+            annotation = SDPropertyCategory(0)
 
-        # ORM nodes will be found via identifier.
-        orm = [None, None, None]
-        output_group = ''
+            # ORM nodes will be found via identifier.
+            orm = [None, None, None]
+            output_group = ''
 
-        for node in outputNodes:
-            identifier = node.getPropertyValueFromId('identifier', annotation).get()
+            for node in outputNodes:
+                identifier = node.getPropertyValueFromId('identifier', annotation).get()
+                
+                # Get the group name from the node for later usage in ORM
+                isgroup = node.getPropertyValueFromId('group', annotation)
+                if (isgroup and isgroup.get() != ''):
+                    output_group = isgroup.get()
+
+                match identifier:
+                    case 'ambientocclusion':
+                        orm[0] = node
+                    case 'roughness':
+                        orm[1] = node
+                    case 'metallic':
+                        orm[2] = node
+                    case _:
+                        continue
             
-            # Get the group name from the node for later usage in ORM
-            isgroup = node.getPropertyValueFromId('group', annotation)
-            if (isgroup and isgroup.get() != ''):
-                output_group = isgroup.get()
+            noEmptySlots = [node for node in orm if node is not None]
+            
+            # If no nodes were found with the identifiers, return
+            if (len(noEmptySlots) <= 0):
+                return
+            # Otherwise, remove all cases of None in the array
+            else:
+                orm = noEmptySlots
 
-            match identifier:
-                case 'ambientocclusion':
-                    orm[0] = node
-                case 'roughness':
-                    orm[1] = node
-                case 'metallic':
-                    orm[2] = node
-                case _:
+            rgba = self.__instanceRgba()
+            rgba_input_props = nds.getNodeProperties(rgba, 1)
+            
+            # Get the input connections for each ORM output node.
+            # These are stored in a dictionary with each output node as a key, 
+            # and the value being a list containing the connections.
+            # (in this case, there is only one connection per node)
+            connections_dict = nds.getInputConnections(orm)
+
+            for i, node in enumerate(orm):
+                # If the node is invalid or has no input connections, skip
+                if (not node or len(connections_dict[node]) <= 0):
                     continue
-        
-        noEmptySlots = [node for node in orm if node is not None]
-        
-        # If no nodes were found with the identifiers, return
-        if (len(noEmptySlots) <= 0):
-            return
-        # Otherwise, remove all cases of None in the array
-        else:
-            orm = noEmptySlots
 
-        rgba = self.__instanceRgba()
-        rgba_input_props = nds.getNodeProperties(rgba, 1)
-        
-        # Get the input connections for each ORM output node.
-        # These are stored in a dictionary with each output node as a key, 
-        # and the value being a list containing the connections.
-        # (in this case, there is only one connection per node)
-        connections_dict = nds.getInputConnections(orm)
+                # Get the first (and only) connection of the current output node,
+                # And get the node / properties connected with it.
+            
+                # For connections:
+                # The input property & node are at the start of the connection.
+                # The output property & node are where the connection ends/leads into.
+                connect_prop = connections_dict[node][0].getInputProperty()
+                connect_node = connections_dict[node][0].getInputPropertyNode()
 
-        for i, node in enumerate(orm):
-            # If the node is invalid or has no input connections, skip
-            if (not node or len(connections_dict[node]) <= 0):
-                continue
+                # For nodes:
+                # The input property is what goes into the node.
+                # The output property is what comes out of the pevious node.
+                #
+                # i.e. sdOutputNode.newPropertyConnection(sdOutputProperty, sdInputPropertyNode, sdInputProperty)
+                #
+                # This is confusing since this is the literal opposite definition of connections' inputs/outputs
+                connect_node.newPropertyConnectionFromId(connect_prop.getId(), rgba, rgba_input_props[i].getId())
 
-            # Get the first (and only) connection of the current output node,
-            # And get the node / properties connected with it.
-        
-            # For connections:
-            # The input property & node are at the start of the connection.
-            # The output property & node are where the connection ends/leads into.
-            connect_prop = connections_dict[node][0].getInputProperty()
-            connect_node = connections_dict[node][0].getInputPropertyNode()
+            # Move the node to an appropriate position
+            # Start by getting a bounding box surrounding all of the original O/R/M output nodes
+            bbox = encapsulate(self.__graphViewId, orm)
 
-            # For nodes:
-            # The input property is what goes into the node.
-            # The output property is what comes out of the pevious node.
-            #
-            # i.e. sdOutputNode.newPropertyConnection(sdOutputProperty, sdInputPropertyNode, sdInputProperty)
-            #
-            # This is confusing since this is the literal opposite definition of connections' inputs/outputs
-            connect_node.newPropertyConnectionFromId(connect_prop.getId(), rgba, rgba_input_props[i].getId())
+            # Set the position of the RGBA Merge to the center right of this box (+250 for spacing)
+            rgba_pos = float2(bbox.top_right().x + 250, bbox.center().y)
+            rgba.setPosition(rgba_pos)
 
-        # Move the node to an appropriate position
-        # Start by getting a bounding box surrounding all of the original O/R/M output nodes
-        bbox = encapsulate(self.__graphViewId, orm)
+            # Create an Output Node with appropriate identifier/label/group/position
+            op_node = self.__addOutput(identifier='ORM', label='ORM', group=output_group)
+            op_node.setPosition(float2(rgba_pos.x + 250, rgba_pos.y))
 
-        # Set the position of the RGBA Merge to the center right of this box (+250 for spacing)
-        rgba_pos = float2(bbox.top_right().x + 250, bbox.center().y)
-        rgba.setPosition(rgba_pos)
+            # Connect the RGBA node to the Output Node
+            rgba_output_prop = nds.getNodeProperties(rgba, 2)[0]
+            op_input_prop = nds.getNodeProperties(op_node, 1)[0]
+            rgba.newPropertyConnectionFromId(rgba_output_prop.getId(), op_node, op_input_prop.getId())
 
-        # Create an Output Node with appropriate identifier/label/group/position
-        op_node = self.__addOutput(identifier='ORM', label='ORM', group=output_group)
-        op_node.setPosition(float2(rgba_pos.x + 250, rgba_pos.y))
-
-        # Connect the RGBA node to the Output Node
-        rgba_output_prop = nds.getNodeProperties(rgba, 2)[0]
-        op_input_prop = nds.getNodeProperties(op_node, 1)[0]
-        rgba.newPropertyConnectionFromId(rgba_output_prop.getId(), op_node, op_input_prop.getId())
-
-        # Focus the Output node
-        self.__sdUIMgr.focusGraphNode(self.__graphViewId, op_node)
+            # Focus the Output node
+            self.__sdUIMgr.focusGraphNode(self.__graphViewId, op_node)
